057a77cde7d1f1e1258c015d42616f7888586d02
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2025-05-15 21:51:10 -0500

    implemented supabase db and services, modularized some code in main.py

--- backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from .game import Game
-from .price_history import PriceHistory
-from .userprofile import UserProfile
-__all__ = ['Base', 'Game', 'PriceHistory', 'UserProfile']
---
+++ backend/supabase_services/game_services.py
@@ -0,0 +1,16 @@
+# game_services.py
+from backend.supabase_client import supabase
+
+def add_game(game_data: dict):
+    result = supabase.table("games").insert(game_data).execute()
+    return result.data
+
+def get_game_by_id(app_id: int):
+    return supabase.table("games").select("*").eq("app_id", app_id).execute()
+
+def get_games():
+    return supabase.table("games").select("*").execute()
+
+def update_game_price(app_id: int, new_price: float, discount_percent: int):
+    result = supabase.table("games").update({"last_known_price": new_price, "discount_percent": discount_percent}).eq("app_id", app_id).execute()
+    return result.data
