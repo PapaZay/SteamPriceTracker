e71eaacdf7297a6d493cc348690a29a94be0bf87
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2025-06-12 02:04:37 -0500

    fixed some bugs and updated logic to insert price history data

--- backend/sync_prices.py
+++ backend/sync_prices.py
@@ -1,13 +1,16 @@
+from locale import currency
+
 from apscheduler.schedulers.background import BackgroundScheduler
 import asyncio
 import logging
 from backend.supabase_services.game_services import get_games, update_game_price
 from backend.api.helper import get_game_data
+from backend.supabase_services.price_history_services import insert_price_history, get_latest_price
 
 scheduler = BackgroundScheduler()
 logger = logging.getLogger("price_sync")
 def start():
-    scheduler.add_job(sync_prices, 'interval', hours=6)
+    scheduler.add_job(sync_prices, 'interval', minutes=1)
     scheduler.start()
     logger.info("Price syncing started.")
 
@@ -20,8 +23,8 @@ async def run_sync_prices():
 
     for game in games.data:
         app_id = game["app_id"]
-        db_price = game.get("last_known_price")
-        db_discount = game.get("discount_percent")
+        game_id = game["id"]
+        currency = game.get("currency")
 
         try:
             steam_api_call = await get_game_data(app_id)
@@ -29,12 +32,26 @@ async def run_sync_prices():
             if not price_info:
                 continue
 
-            new_price = price_info["final"] / 100
+            new_current = price_info["final"] / 100
+            new_initial = price_info["initial"] / 100
             new_discount = price_info["discount_percent"]
 
-            if new_price != db_price or new_discount != db_discount:
-                update_game_price(app_id, new_price=new_price, discount_percent=new_discount)
-                logger.info(f"Updated {game['name']} to ${new_price} with {new_discount}% off.")
+            latest = get_latest_price(game_id)
+            latest_current = float(latest["final_price"]) if latest else None
+            latest_discount = int(latest["discount_percent"]) if latest else None
+
+            if new_current != latest_current or new_discount != latest_discount:
+
+                insert_price_history(
+                    game_id=game_id,
+                    initial_price=new_initial,
+                    final_price=new_current,
+                    discount_percent=new_discount,
+                    currency=currency
+
+                )
+                update_game_price(app_id, new_price=new_current, discount_percent=new_discount)
+                logger.info(f"logged price history change for {game['name']} (${new_current}, {new_discount}% off).")
         except Exception as e:
             logger.error(f"Error syncing game {app_id}: {e}")
-    logger.info("Syncing complete.")
+    #logger.info("Syncing complete.")
