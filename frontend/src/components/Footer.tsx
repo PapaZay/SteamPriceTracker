3b652eb48b47b934176aaedc641222d1b35e3c3d
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2025-07-29 17:40:43 -0500

    added some social links in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,19 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+
 const Footer = () => {
     return (
         <footer className="bg-white dark:bg-darkblue border-t border-gray-200 dark:border-gray-700 mt-auto">
             <div className="max-w-7xl mx-auto px-4 py-6">
+                <div className="flex justify-center items-center text-3xl mb-2 space-x-4 text-gray-600 dark:text-gray-400">
+                    <a href="https://www.linkedin.com/in/isaiahflagerhearan/" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400">
+                    <FaLinkedin />
+                    </a>
+                    <a href="https://github.com/PapaZay" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400">
+                    <FaGithub/>
+                    </a>
+                </div>
                 <div className="text-center text-sm text-gray-600 dark:text-gray-400">
                     © 2025 SteamPriceTracker. Built by Isaiah Flager-Hearan
                 </div>
