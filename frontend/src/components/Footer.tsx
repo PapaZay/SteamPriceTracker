4c90651e0826cf2cff7c4ad4db2df6b193d1d99c
Isaiah Flager-Hearan <94151429+PapaZay@users.noreply.github.com>
2025-09-14 23:08:44 -0500

    updated hovers on footer, specifically for dark mode

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,10 +7,10 @@ const Footer = () => {
         <footer className="bg-white dark:bg-darkblue border-t border-gray-200 dark:border-gray-700 mt-auto">
             <div className="max-w-7xl mx-auto px-4 py-6">
                 <div className="flex justify-center items-center text-3xl mb-2 space-x-4 text-gray-600 dark:text-gray-400">
-                    <a href="https://www.linkedin.com/in/isaiahflagerhearan/" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400">
+                    <a href="https://www.linkedin.com/in/isaiahflagerhearan/" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 transition-colors">
                     <FaLinkedin />
                     </a>
-                    <a href="https://github.com/PapaZay" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400">
+                    <a href="https://github.com/PapaZay" target="_blank" rel="noopener noreferrer" className="text-gray-600 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 transition-colors">
                     <FaGithub/>
                     </a>
                 </div>
